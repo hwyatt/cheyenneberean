ddbc4b2a650b6d2f87eeae96b831fcc2abc607a3
Hunter Wyatt <huntercwyatt@gmail.com>
2024-08-06 22:59:01 -0600

    values

--- src/app/kids/page.tsx
+++ src/app/kids/page.tsx
@@ -13,27 +13,32 @@ const VALUES = [
   {
     title: "Gospel Centered",
     desc: "Our lessons come directly from Scripture. Our curriculum and conversations regularly establish our need for Jesus both to save us from our sins and to live courageously for Him.",
-    icon: <PiCross className="h-8 w-8 text-gray-800" />,
+    icon: <PiCross className="h-8 w-8 text-orange-600" />,
+    bgColor: "#6A7065",
   },
   {
     title: "Partnership",
     desc: "Scripture stresses the importance of parents and the local church in spiritual formation. We commit to partnering together; equipping and releasing a team of people who want to see children's lives transformed.",
-    icon: <LuHeartHandshake className="h-8 w-8 text-gray-800" />,
+    icon: <LuHeartHandshake className="h-8 w-8 text-orange-600" />,
+    bgColor: "#6A7065",
   },
   {
     title: "Safety",
     desc: "We use structured and thorough safety protocols. All volunteers are consistently screened to ensure maximum safety and security for families.",
-    icon: <AiOutlineSafety className="h-8 w-8 text-gray-800" />,
+    icon: <AiOutlineSafety className="h-8 w-8 text-orange-600" />,
+    bgColor: "#6A7065",
   },
   {
     title: "Excellence",
     desc: "We provide quality teaching and instruction to kids of all ages. We strive to be organized, consistent, and impactful so the Gospel can reach kids in lasting ways.",
-    icon: <LiaCheckCircle className="h-8 w-8 text-gray-800" />,
+    icon: <LiaCheckCircle className="h-8 w-8 text-orange-600" />,
+    bgColor: "#6A7065",
   },
   {
     title: "Age Specific",
     desc: "Our curriculum runs chronologically through the Bible to help a child understand the Bible as a whole. We commit to helping even the youngest of our congregation become resilient followers of Jesus.",
-    icon: <TbMoodKid className="h-8 w-8 text-gray-800" />,
+    icon: <TbMoodKid className="h-8 w-8 text-orange-600" />,
+    bgColor: "#6A7065",
   },
 ];
 
@@ -57,7 +62,7 @@ const CLASSES = [
     ages: "1st-3rd Grade",
     desc: "This class will equip your child to participate in conversations, learn the Gospel through discussion, reading, memorizing Scripture, and games.",
     image: "",
-    bgColor: "#6A7065",
+    bgColor: "#4C5048",
   },
   {
     name: "Anchor",
@@ -110,21 +115,56 @@ const KidsPage = async ({}) => {
         />
       )}
       <div className="flex flex-col items-center gap-8 w-full">
-        <div className="flex flex-col items-center gap-2 copy-container">
-          <h2 className="text-lg font-semibold uppercase">Values</h2>
-          <div className="flex flex-col items-center text-center gap-4">
+        <div className="flex flex-col items-center gap-2">
+          {/* <h2 className="text-lg font-semibold uppercase">Values</h2> */}
+          <div className="flex flex-col items-center text-center gap-4 md:grid md:grid-cols-3">
             {VALUES.map((value) => (
+              // <div
+              //   className="flex flex-col gap-2 items-center text-center"
+              //   key={value.title}
+              // >
+              //   <div className="flex items-center text-center gap-2">
+              //     {value.icon}
+              //     <span className="text-gray-800 font-semibold">
+              //       {value.title}
+              //     </span>
+              //   </div>
+              //   <div className="text-center">{value.desc}</div>
+              // </div>
+
               <div
-                className="flex flex-col gap-2 items-center text-center"
-                key={value.title}
+                className="
+              font-semibold
+              p-4
+              md:px-8
+              md:pb-8
+              md:pt-16
+              rounded
+              flex flex-col
+              gap-4
+              justify-between
+              relative
+              overflow-hidden
+              text-start
+              md:col-span-1
+              h-full
+              shadow-md
+            "
+                style={{ background: value.bgColor }}
               >
-                <div className="flex items-center text-center gap-2">
-                  {value.icon}
-                  <span className="text-gray-800 font-semibold">
-                    {value.title}
-                  </span>
+                <img
+                  src="/dark-mountains-kids.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+                <div className="flex flex-col gap-1 z-10">
+                  <div className="flex flex-col gap-4">
+                    {value.icon}
+                    <span className="text-xl text-white z-10">
+                      {value.title}
+                    </span>
+                  </div>
+                  <p className="text-white font-light z-10">{value.desc}</p>
                 </div>
-                <div className="text-center">{value.desc}</div>
               </div>
             ))}
           </div>
