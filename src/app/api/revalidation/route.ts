fb32e495e3b9ca52091caf1fbaa2e5e9ef49dd8d
Hunter Wyatt <huntercwyatt@gmail.com>
2024-11-12 21:51:45 -0700

    test reval

--- src/app/api/revalidation/route.ts
+++ src/app/api/revalidation/route.ts
@@ -9,23 +9,14 @@ export async function POST(request: any) {
   //     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   //   }
 
-  //   revalidateTag("event");
-
-  // Parse the JSON payload from Contentful
   const body = await request.json();
-
-  // Extract contentType and entry id
   const contentType = body.sys?.contentType?.sys?.id;
-  const entryId = body.sys?.id;
 
-  // Ensure we have the necessary data for revalidation
-  if (contentType && entryId) {
-    revalidateTag(`${contentType}:${entryId}`); // Specific revalidation for this entry
-  } else if (contentType) {
-    revalidateTag(contentType); // General revalidation for this content type
+  if (contentType) {
+    revalidateTag(contentType);
   } else {
     return NextResponse.json(
-      { message: "Missing content type or entry ID" },
+      { message: "Missing content type" },
       { status: 400 }
     );
   }
