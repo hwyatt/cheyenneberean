5c2e94b66f089c5118d43164de70caf4a4f08dcf
Hunter Wyatt <huntercwyatt@gmail.com>
2024-11-18 15:01:39 -0700

    test

--- src/app/api/revalidation/route.ts
+++ src/app/api/revalidation/route.ts
@@ -22,6 +22,6 @@ export async function POST(request: any) {
     );
   }
 
-  draftMode().disable();
+  // draftMode().disable();
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
