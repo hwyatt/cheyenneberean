5c187094c80b8202730d12f3063abb6c90dc2426
Hunter Wyatt <huntercwyatt@gmail.com>
2024-11-20 12:22:33 -0700

    Fix contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,56 +1,61 @@
 "use client";
-import { useState, Suspense } from "react";
+import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { IntroSection } from "../components/IntroSection/IntroSection";
 import { FaPhone } from "react-icons/fa6";
 
-const getMessage = (referrer: string | null) => {
-  switch (referrer) {
-    case "prayer":
-      return {
-        heading: "Prayer Request",
-        description:
-          "We believe in the power of prayer. Let us know how we can pray for you.",
-      };
-    case "kids":
-      return {
-        heading: "Contact BereanKIDS",
-        description:
-          "We would love to connect with you on any questions you may have about BereanKIDS.",
-      };
-    case "youth":
-      return {
-        heading: "Contact The Refuge",
-        description:
-          "We would love to connect with you on any questions you may have about The Refuge.",
-      };
-    case "young-adults":
-      return {
-        heading: "Contact Crossroads 412",
-        description:
-          "We would love to connect with you on any questions you may have about Crossroads 412.",
-      };
-    case "awana":
-      return {
-        heading: "Contact Us",
-        description:
-          "We would love to connect with you on any questions you may have about Awana at Cheyenne Berean Church.",
-      };
-    default:
-      return {
-        heading: "Contact Us",
-        description:
-          "We would love to connect with you on any questions you have, or help you get plugged into church life at Cheyenne Berean Church.",
-      };
-  }
-};
+// const getMessage = (referrer: string | null) => {
+//   switch (referrer) {
+//     case "prayer":
+//       return {
+//         heading: "Prayer Request",
+//         description:
+//           "We believe in the power of prayer. Let us know how we can pray for you.",
+//       };
+//     case "kids":
+//       return {
+//         heading: "Contact BereanKIDS",
+//         description:
+//           "We would love to connect with you on any questions you may have about BereanKIDS.",
+//       };
+//     case "youth":
+//       return {
+//         heading: "Contact The Refuge",
+//         description:
+//           "We would love to connect with you on any questions you may have about The Refuge.",
+//       };
+//     case "young-adults":
+//       return {
+//         heading: "Contact Crossroads 412",
+//         description:
+//           "We would love to connect with you on any questions you may have about Crossroads 412.",
+//       };
+//     case "awana":
+//       return {
+//         heading: "Contact Us",
+//         description:
+//           "We would love to connect with you on any questions you may have about Awana at Cheyenne Berean Church.",
+//       };
+//     default:
+//       return {
+//         heading: "Contact Us",
+//         description:
+//           "We would love to connect with you on any questions you have, or help you get plugged into church life at Cheyenne Berean Church.",
+//       };
+//   }
+// };
 
 const ContactPage = () => {
-  const searchParams = useSearchParams();
-  const referrer = searchParams.get("referrer");
-  const message = referrer
-    ? getMessage(referrer)
-    : { heading: "Contact Us", description: "" };
+  // const searchParams = useSearchParams();
+  // const referrer = searchParams.get("referrer");
+  // const message = referrer
+  //   ? getMessage(referrer)
+  //   : { heading: "Contact Us", description: "" };
+  const message = {
+    heading: "Contact Us",
+    description:
+      "We would love to connect with you on any questions you have, or help you get plugged into church life at Cheyenne Berean Church.",
+  };
 
   const [formData, setFormData] = useState({
     name: "",
@@ -93,86 +98,78 @@ const ContactPage = () => {
   };
 
   return (
-    <Suspense
-      fallback={
-        <div className="min-h-screen flex flex-col items-center gap-8 md:gap-16">
-          Loading...
-        </div>
-      }
-    >
-      <div className="min-h-screen flex flex-col items-center gap-8 md:gap-16">
-        <IntroSection
-          heading={message.heading}
-          description={message.description}
-        />
-        {!status && (
-          <form
-            onSubmit={handleSubmit}
-            className="w-full md:w-1/2 flex flex-col gap-8 text-primary"
-          >
-            <label className="input input-bordered border-accent flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-              </svg>
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="grow text-gray-800"
-                placeholder="Name"
-              />
-            </label>
-            <label className="input input-bordered border-accent flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4"
-              >
-                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-              </svg>
-              <input
-                type="text"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="grow text-gray-800"
-                placeholder="Email"
-              />
-            </label>
-            <label className="input input-bordered border-accent flex items-center gap-2">
-              <FaPhone fill="currentColor" className="h-4 w-4" />
-              <input
-                type="text"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                className="grow text-gray-800"
-                placeholder="Phone Number"
-              />
-            </label>
-            <textarea
-              name="message"
-              value={formData.message}
+    <div className="min-h-screen flex flex-col items-center gap-8 md:gap-16">
+      <IntroSection
+        heading={message.heading}
+        description={message.description}
+      />
+      {!status && (
+        <form
+          onSubmit={handleSubmit}
+          className="w-full md:w-1/2 flex flex-col gap-8 text-primary"
+        >
+          <label className="input input-bordered border-accent flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+            </svg>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
               onChange={handleChange}
-              placeholder="Message"
-              className="textarea-placeholder textarea textarea-bordered border-accent textarea-lg min-h-64 text-gray-800"
-            ></textarea>
-            <button type="submit" className="btn btn-accent">
-              Submit
-            </button>
-          </form>
-        )}
-        {status && <p className="font-semibold text-lg">{status}</p>}
-      </div>
-    </Suspense>
+              className="grow text-gray-800"
+              placeholder="Name"
+            />
+          </label>
+          <label className="input input-bordered border-accent flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="grow text-gray-800"
+              placeholder="Email"
+            />
+          </label>
+          <label className="input input-bordered border-accent flex items-center gap-2">
+            <FaPhone fill="currentColor" className="h-4 w-4" />
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className="grow text-gray-800"
+              placeholder="Phone Number"
+            />
+          </label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Message"
+            className="textarea-placeholder textarea textarea-bordered border-accent textarea-lg min-h-64 text-gray-800"
+          ></textarea>
+          <button type="submit" className="btn btn-accent">
+            Submit
+          </button>
+        </form>
+      )}
+      {status && <p className="font-semibold text-lg">{status}</p>}
+    </div>
   );
 };
 
