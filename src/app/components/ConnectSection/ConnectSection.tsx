2e59a4156c6c97a5065944a18d627d41721cf136
Hunter Wyatt <huntercwyatt@gmail.com>
2024-11-29 10:10:06 -0700

    connect section Image

--- src/app/components/ConnectSection/ConnectSection.tsx
+++ src/app/components/ConnectSection/ConnectSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ConnectSectionProps } from "@/app/api/types";
 import { Button } from "../Button/Button";
 
@@ -15,7 +16,17 @@ export const ConnectSection = ({
     } border-t-2 border-b-2 w-full`}
   >
     <div className="flex flex-col md:flex-row items-center gap-4">
-      {logo && <img src={logo.url} className="max-h-12 mb-4 md:mb-0" />}
+      {logo && (
+        <Image
+          src={logo.url}
+          alt={heading}
+          className="mb-4 md:mb-0"
+          width={126}
+          height={48}
+          objectFit="contain"
+          style={{ maxHeight: "48px", width: "auto" }}
+        />
+      )}
       <div className="flex flex-col gap-2 mb-4 md:mb-0">
         <span
           className={`text-center md:text-start font-medium text-dark text-2xl md:leading-5 text-balance`}
