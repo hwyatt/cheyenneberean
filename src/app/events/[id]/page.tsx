dc020c6d17094ac36c29a9018c34eae2920fcc2e
Hunter Wyatt <huntercwyatt@gmail.com>
2024-12-04 10:48:27 -0700

    event deets fix

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -18,8 +18,9 @@ const EventDetailsPage = async ({ params }: EventPageProps) => {
   const event = await fetchEvents(id);
 
   return (
-    <div className="min-h-screen flex flex-col items-center gap-8">
-      {/* <GMap
+    event && (
+      <div className="min-h-screen flex flex-col items-center gap-8">
+        {/* <GMap
         zoom={14}
         center={{ lat: event.location?.lat, lng: event.location?.lon }}
         mapStyle={{
@@ -28,43 +29,45 @@ const EventDetailsPage = async ({ params }: EventPageProps) => {
           marginTop: "-32px",
         }}
       /> */}
-      <div className="flex flex-col w-full gap-8">
-        <div className="flex flex-col md:flex-row justify-between md:items-center border-borderPrimary border-b-2 gap-4 pb-4">
-          <div className="flex flex-col gap-2">
+        <div className="flex flex-col w-full gap-8">
+          <div className="flex flex-col md:flex-row justify-between md:items-center border-borderPrimary border-b-2 gap-4 pb-4">
             <div className="flex flex-col gap-2">
-              {[event.event.group.name] && (
-                <div className="flex gap-2">
-                  {[event.event.group.name].map(
-                    (item: CCBEventResponse["event"]["group"]["name"]) => (
-                      <Chip key={item[0]}>{item}</Chip>
-                    )
-                  )}
-                </div>
-              )}
-              <h1 className="text-dark font-medium text-2xl md:text-3xl text-balance">
-                {event.event.name}
-              </h1>
+              <div className="flex flex-col gap-2">
+                {[event.event.group.name] && (
+                  <div className="flex gap-2">
+                    {[event.event.group.name].map(
+                      (item: CCBEventResponse["event"]["group"]["name"]) => (
+                        <Chip key={item[0]}>{item}</Chip>
+                      )
+                    )}
+                  </div>
+                )}
+                <h1 className="text-dark font-medium text-2xl md:text-3xl text-balance">
+                  {event.event.name}
+                </h1>
+              </div>
+            </div>
+
+            <div className="flex gap-4">
+              <Button href={event.registrationLink}>Register Now</Button>
+              <Button variant="Secondary">
+                <PiShareFat size="20" />
+              </Button>
             </div>
           </div>
-
-          <div className="flex gap-4">
-            <Button href={event.registrationLink}>Register Now</Button>
-            <Button variant="Secondary">
-              <PiShareFat size="20" />
-            </Button>
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row gap-8">
-          <div className="flex flex-col gap-1 md:w-1/3">
-            {event.start && (
-              <div className="flex gap-2 items-center">
-                <FaRegClock className="text-body text-lg" />
-                <span className="text-body text-base">
-                  {moment(event.start).format("dddd, MMMM D, YYYY [at] h:mm A")}
-                </span>
-              </div>
-            )}
-            {/* {event.event.owner && (
+          <div className="flex flex-col md:flex-row gap-8">
+            <div className="flex flex-col gap-1 md:w-1/3">
+              {event.start && (
+                <div className="flex gap-2 items-center">
+                  <FaRegClock className="text-body text-lg" />
+                  <span className="text-body text-base">
+                    {moment(event.start).format(
+                      "dddd, MMMM D, YYYY [at] h:mm A"
+                    )}
+                  </span>
+                </div>
+              )}
+              {/* {event.event.owner && (
               <div className="flex gap-2 items-center">
                 <IoPersonOutline className="text-body text-lg" />
                 <span className="text-body text-base m-0">
@@ -72,19 +75,22 @@ const EventDetailsPage = async ({ params }: EventPageProps) => {
                 </span>
               </div>
             )} */}
-          </div>
-          {event.event.description && (
-            <div className="flex flex-col gap-1 md:w-2/3 text-balance">
-              <h2 className="font-medium text-lg md:text-xl">Description</h2>
-              <p className="text-body md:text-lg">{event.event.description}</p>
             </div>
-          )}
+            {event.event.description && (
+              <div className="flex flex-col gap-1 md:w-2/3 text-balance">
+                <h2 className="font-medium text-lg md:text-xl">Description</h2>
+                <p className="text-body md:text-lg">
+                  {event.event.description}
+                </p>
+              </div>
+            )}
+          </div>
+          <Button variant="Secondary" className="self-center" href="/events">
+            <span className="text-inherit">See All Events</span>
+          </Button>
         </div>
-        <Button variant="Secondary" className="self-center" href="/events">
-          <span className="text-inherit">See All Events</span>
-        </Button>
       </div>
-    </div>
+    )
   );
 };
 
